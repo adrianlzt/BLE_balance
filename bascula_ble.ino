cf84f976d3df5fe7fd494d602ac50dd4598d6235
Adrian Lopez <adrianlzt@gmail.com>
2020-11-27 19:39:09 +0100

    First commit

---
+++ bascula_ble.ino
@@ -0,0 +1,133 @@
+/*
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second.
+*/
+#include <string.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include "HX711.h"
+
+// Initialize our HX711 interface
+HX711 scale;
+const float calibration_factor = -113.00;
+const int LOADCELL_DOUT_PIN = 18;
+const int LOADCELL_SCK_PIN = 21;
+const int LOADCELL_OFFSET = -155412;
+const int LOADCELL_SCALE = 22;
+
+BLECharacteristic *pCharacteristic;
+bool deviceConnected = false;
+bool wasConnected = false;
+float txValue = 0;
+const int readPin = 32; // Use GPIO number. See ESP32 board pinouts
+
+//std::string rxValue; // Could also make this a global var to access it in loop()
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_offset(LOADCELL_OFFSET);
+  scale.set_scale(LOADCELL_SCALE);
+
+  // Create the BLE Device
+  BLEDevice::init("Bascula BLE");
+
+  // Give it a name
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_TX,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // La app movil no muestra datos si no tenemos esta línea, no he investigado por que.
+  // btgatt-client si muestra notificationes sin esta linea
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID_RX,
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...\n");
+}
+
+void loop() {
+  if (deviceConnected) {
+    if (!wasConnected) {
+      Serial.print("Connected\n");
+      scale.power_up();
+      wasConnected = true;
+    }
+
+    Serial.print("*** Sent Value: ");
+
+    if (scale.is_ready()) {
+      char txString[8];
+      long reading = scale.get_units(10);
+
+      dtostrf(reading, 1, 0, txString); // float_val, min_width, digits_after_decimal, char_buffer
+      strcat(txString, "\n");
+      pCharacteristic->setValue(txString);
+      pCharacteristic->notify(); // Send the value to the app!
+      Serial.print(reading);
+    } else {
+      pCharacteristic->setValue("no data\n");
+      Serial.print("no data");
+    }
+    Serial.print("\n");
+
+  } else {
+    if (wasConnected) {
+      Serial.print("Disconnected\n");
+      scale.power_down();
+      wasConnected = false;
+    }
+  }
+
+  delay(1000);
+}
---
+++ upload.sh
@@ -0,0 +1,1 @@
+arduino-cli upload --fqbn esp32:esp32:esp32doit-devkit-v1 -p /dev/ttyUSB0
