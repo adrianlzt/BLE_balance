1ce4444057c4cb0abcf0f86952345ee162d9d383
Adrian Lopez <adrianlzt@gmail.com>
2021-04-07 20:36:05 +0200

    Version funcional con datos más útiles
    Se ajustan algunos parámetros.
    Se muestra por pantalla el peso medido (tanque), el de gas (total-tanque
    vacio) y el porcentaje de llenado.

--- bascula_ble.ino
+++ bascula_ble.ino
@@ -1,8 +1,12 @@
 /*
  * Bascula que expone los datos mediante BLE
+ * Podemos usar la app https://play.google.com/store/apps/details?id=com.mightyit.gops.bleterminal&hl=en_US&gl=US para leer los datos
+ * IMPORTANTE: recordar desconectar la app del móvil tras leer los datos
+ *
+ * Se calcula el peso de gas y porcentaje de llenado (respecto al 80% máximo de la botella) asumiendo que los valores void_tank y tank_capacity son correctos
  */
 
-#include <string.h>
+#include <stdio.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
@@ -13,8 +17,15 @@
 HX711 scale;
 const int LOADCELL_DOUT_PIN = 18;
 const int LOADCELL_SCK_PIN = 21;
-const int LOADCELL_OFFSET = -151562;
+const int LOADCELL_OFFSET = -153322;
+//const int LOADCELL_OFFSET = -151562; // -80gr
+//const int LOADCELL_OFFSET = -149802;
 const int LOADCELL_SCALE = 22;
+
+// Peso del tanque de gas en vacío, en gramos
+const int void_tank = 12400;
+// capacidad del tanque, en gramos
+const int tank_capacity = 11760;
 
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
@@ -84,16 +95,47 @@ void loop() {
     Serial.print("Sent Value: ");
 
     if (scale.is_ready()) {
-      char txString[8];
-      long reading = scale.get_units(10);
+      char txString[50];
+      char tank_weight_str[12];
+      char gas_weight_str[12];
+      char gas_pct_str[12];
 
-      dtostrf(reading, 1, 0, txString); // float_val, min_width, digits_after_decimal, char_buffer
-      strcat(txString, "\n");
+      // peso total del tanque
+      long tank_weight = scale.get_units(10);
+      dtostrf(tank_weight, 1, 0, tank_weight_str);
+
+      sprintf(txString, "tank  %s gr\n", tank_weight_str);
+      pCharacteristic->setValue(txString);
+      pCharacteristic->notify(); // send the value to the app!
+
+      // peso del gas almacenado en el tanque
+      long gas_weight = tank_weight - void_tank;
+      dtostrf(gas_weight, 1, 0, gas_weight_str);
+
+      sprintf(txString, "gas   %s gr\n", gas_weight_str);
+      pCharacteristic->setValue(txString);
+      pCharacteristic->notify(); // send the value to the app!
+
+      // porcentaje de llenado (100% es que el tanque está a su máximo de 80%, el 20% restante es de seguridad)
+      long gas_pct = gas_weight * 100 / tank_capacity;
+      dtostrf(gas_pct, 1, 1, gas_pct_str);
+
+      sprintf(txString, "pct    %s %%\n---\n", gas_pct_str);
       pCharacteristic->setValue(txString);
       pCharacteristic->notify(); // Send the value to the app!
-      Serial.print(reading);
+
+      Serial.print("tank=");
+      Serial.print(tank_weight);
+      Serial.print(" gr  ");
+      Serial.print("gas=");
+      Serial.print(gas_weight);
+      Serial.print(" gr  ");
+      Serial.print("pct=");
+      Serial.print(gas_pct);
+      Serial.print(" %  ");
     } else {
       pCharacteristic->setValue("no data\n");
+      pCharacteristic->notify(); // Send the value to the app!
       Serial.print("no data");
     }
     Serial.print("\n");
