7bcbd245db837c467ef98d58ed6a0d7e6b1b41e0
Adrian Lopez <adrianlzt@gmail.com>
2021-12-11 18:56:14 +0100

    Intento de hacer de termómetro y báscula al mismo tiempo
    Parece que el ble monitor no le convence.
    Tampoco parece que haya una manera de cambiar la mac para poder simular
    ser dos dispositivos.

--- micropython/main.py
+++ micropython/main.py
@@ -1,3 +1,5 @@
+import esp32
+import random
 import ubluetooth
 from machine import Pin, Timer
 from time import sleep_ms
@@ -12,6 +14,10 @@ class BLE():
         self.led = Pin(2, Pin.OUT)
         self.timer1 = Timer(0)
         self.timer2 = Timer(1)
+        self.timer3 = Timer(2)
+
+        # Modificamos el advertiser cada segundo
+        self.timer3.init(period=1000, mode=Timer.PERIODIC, callback=lambda _: self.advertiser())
 
         self.disconnected()
         self.advertiser()
@@ -28,15 +34,51 @@ class BLE():
     def advertiser(self):
         data = b'\x02\x01\x06' # flags: BR/EDR Not Supported + LE General Discoverable Mode
 
-        data += b'\x0d' # length (type + pack)
-        data += b'\x16' # Type: Service Data - 16 bit UUID (0x16)
-        data += b'\x1d\x18' # UUID 16: Weight Scale (0x181d)
+        # Hacer que flapee entre uno y otro
+        # TODO el problema es que el módulo de HA parece que espera o una cosa o la otra
+        r = random.getrandbits(1)
+        r = 0
 
-        peso = 99.999
-        data += b'\x20' # stabilized weight
-        data += pack('H', int(peso*200)) # peso
-        data += b'\x00\x00\x00\x00\x00\x00\x00\x00'
+        if r == 0:
+            # simular bascula
+            data += b'\x0d' # length (type + pack)
+            data += b'\x16' # Type: Service Data - 16 bit UUID (0x16)
+            data += b'\x1d\x18' # UUID 16: Weight Scale (0x181d)
 
+            peso = (random.getrandbits(14)/1000)
+            data += b'\x20' # stabilized weight
+            data += pack('H', int(peso*200)) # peso
+            data += b'\x00\x00\x00\x00\x00\x00\x00\x00'
+
+        else:
+            # Simular ser un termómetro
+
+            # adstruct
+            data += b'\x15' # length (type + pack)
+            data += b'\x16' # Type: Service Data - 16 bit UUID (0x16)
+            data += b'\x95\xfe' # UUID Xiaomi
+            data += b'\x50\x20' # [4,5] frctrl
+            data += b'\xaa\x01' # [6,7] device id
+            data += b'\xda' # [8] packet id
+            data += b'\xe2\x5d\x00\x98\x83\x30' # [9,10,11,12,13,14] mac reversed
+
+            # payload
+            data += b'\x0d\x10' # obj_typecode (reversed, 0x100d)
+            data += b'\x04' # length
+
+            temp = esp32.raw_temperature()
+            # Convert farhenheit to celcius
+            temp = (temp - 32) * 5/9
+
+            #data += b'\xfe\x00' # temperature, reversed, multiplicada por 10 (0x00fe = 254 -> 25.4C)
+            data += pack('<h', int(temp*10)) # temperature, reversed, multiplicada por 10 (0x00fe = 254 -> 25.4C)
+
+            data += b'\x48\x02' # humedad, reversed, multiplicada por 10
+
+            data += b'\xc4'
+
+
+        #print(f"gap_advertise: {bytearray(data)}")
         self.ble.gap_advertise(500000, bytearray(data))
 
 ble = BLE("ESP32")
